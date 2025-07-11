5d4b94440b07fcf3d65d038454910d309ebe8e58
Md Mubin <fontdevmubin@gmail.com>
2025-07-11 12:28:54 +0600

    added cookie sesionm

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 require('dotenv').config()
 const express = require("express")
+const cookieSession = require("cookie-session")
+const passport = require("passport")
+const session = require("express-session")
 require("./models/userSchema")
 require("./services/passport")
 
 const app = express()
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys : ["asdasdaskhfdula"]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 require('./config/db')
 require("./router/authRoute")(app)
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -4,6 +4,15 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy
 
 const User = mongoose.model("users")
 
+passport.serializeUser((user,done)=>{
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done)=>{
+    User.findById(id)
+    .then(user=> done(null, user))
+})
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET_ID,
