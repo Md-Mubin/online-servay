f4e78e26b2acc5ac1d4e28cee18f03729be04fc8
Md Mubin <fontdevmubin@gmail.com>
2025-07-11 11:00:27 +0600

    _added mongodb database'

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGO_DATABASE_URL).then(()=>console.log("Database Connected"))
+
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express")
 require("./helpers.js/passport")
 
 const app = express()
+require('./config/db')
 require("./router/authRoute")(app)
 
 app.listen(8000)
